2acb15d139ffee12894f46d35c5bf6fff1132aab
Nicolás Kivatinetz <nkivatinetz@truelogic.io>
2022-10-20 22:33:22 -0300

    backend

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const reservations = [];
+
+const isReservationValid = (reservation) => !!reservation.vehicleId && !!reservation.userId;
+app.post("/reservations", (req, res) => {
+  const reservation = req.body;
+
+  if (!isReservationValid(reservation)) {
+    res.status(400);
+    res.end();
+    return;
+  }
+  reservations.push(req.body);
+
+  res.status(201);
+  res.end();
+});
+
+app.get("/reservations", (req, res) => {
+  res.json(reservations);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
